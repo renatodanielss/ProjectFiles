cbb072f881b77cd33d19d1c695956bd7b2ec47b2
renatodanielss <renatodanielss@gmail.com>
2018-05-01 22:26:26 -0300

    -Adição de comentários na classe MainClass

--- ProjectFiles/src/br/com/cts/main/MainClass.java
+++ ProjectFiles/src/br/com/cts/main/MainClass.java
@@ -1,3 +1,9 @@
+/*
+ * Classe: MainCLass
+ * Descrição: Classe de teste dos métodos da classe FileHandler
+ * Autor: Renato Daniel Santana Santos
+ */
+
 package br.com.cts.main;
 
 import java.io.IOException;
@@ -14,24 +20,24 @@ public class MainClass {
 		dir = dir.replaceFirst("/", "//");
 		filesList(dir);*/
 		
-		String dir = JOptionPane.showInputDialog("Digite o diretório:");
-		String alterTag = JOptionPane.showInputDialog("Digite as alterações que deseja fazer:");
+		String dir = JOptionPane.showInputDialog("Digite o diretório:"); //Chama inputbox para inserir diretório
+		String alterTag = JOptionPane.showInputDialog("Digite as alterações que deseja fazer:"); //Chama inputbox para informar as alterações que se deseja realizar
 		
+		//Teste se o diretório foi digitado
 		if (!dir.equals(null) && !alterTag.equals(null) && !dir.equals("") && !alterTag.equals("")){
-			dir = dir.replace("\\", "//");
-			
+			dir = dir.replace("\\", "//"); //Substitui toda barra invertida (padrão de diretório do Windows) por barra (padrão de diretório do Java)
 			try {
-				//Opção 1: realizar as substituições definidas no aquivo FileHandler.java
+				//Opção 1: realizar as substituições no nome do arquivo musical
 				if (alterTag.indexOf('1') > -1){
 					startSetFileChar(dir);
 					Thread.sleep(2000);
 				}
-				//Opção 2: colocar primeira letra de cada palavra do nome do arquivo em maiúscula
+				//Opção 2: colocar primeira letra de cada palavra do nome do arquivo musical em maiúscula
 				if (alterTag.indexOf('2') > -1){
 					startSetFileUpper(dir);
 					Thread.sleep(2000);
 				}
-				//Opção 3: colocar primeira letra de cada palavra da propriedade title em maiúscula
+				//Opção 3: colocar primeira letra de cada palavra da tag title em maiúscula
 				if (alterTag.indexOf('3') > -1){
 					startSetEachTitleFirstLetter(dir);
 					Thread.sleep(2000);
@@ -57,30 +63,35 @@ public class MainClass {
 		}
 	}
 	
+	//Chama o método replaceStringInFiles da classe FileHandler
 	public static void startSetFileChar(String dir){
 		FileHandler fileHandler = new FileHandler(dir);
 		fileHandler.replaceStringInFiles();
 		System.out.println("startSetFileChar Finished");
 	}
 	
+	//Chama o método eachFirstLetterUpperCase da classe FileHandler
 	public static void startSetFileUpper(String dir){
 		FileHandler fileHandler = new FileHandler(dir);
 		fileHandler.eachFirstLetterUpperCase();
 		System.out.println("startSetFileUpper Finished");
 	}
 	
+	//Chama o método eachSongTitleFirstLetterUpperCase da classe FileHandler
 	public static void startSetEachTitleFirstLetter(String dir) throws IOException, TagException{
 		FileHandler fileHandler = new FileHandler(dir);
 		fileHandler.eachSongTitleFirstLetterUpperCase();
 		System.out.println("startSetEachTitleFirstLetter Finished");
 	}
 	
+	//Chama o método eachSongTitleFromFileName da classe FileHandler
 	public static void startSetEmptyName(String dir) throws IOException, TagException{
 		FileHandler fileHandler = new FileHandler(dir);
 		fileHandler.eachSongTitleFromFileName();
 		System.out.println("startSetEmptyName Finished");
 	}
 	
+	//Chama o método eachSongNumber da classe FileHandler
 	public static void startSetSongNumber(String dir) throws IOException, TagException{
 		FileHandler fileHandler = new FileHandler(dir);
 		fileHandler.eachSongNumber();
