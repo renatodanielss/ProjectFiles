96fb76defd7b51818905b3ea3c1c9815b3cb30e9
renatodanielss <renatodanielss@gmail.com>
2018-12-27 18:22:05 -0200

    Ignorar conchetes ao alterar letras para maiúsculas.

--- ProjectFiles/src/br/com/cts/model/FileHandler.java
+++ ProjectFiles/src/br/com/cts/model/FileHandler.java
@@ -78,16 +78,16 @@ public class FileHandler {
 			if(musica.substring(musica.length()-4, musica.length()).toLowerCase().equals(".mp3")){
 				//BEGIN - REPLACES
 				//musica = musica.replaceFirst(musica.substring(0, 2), "");
-//				musica = musica.replaceFirst("-fts", "");
+//				musica = musica.replaceFirst("Cat Power -Clear the Room-Rarities- ", "");
 				//musica = musica.replaceFirst("\\[", "\\(");
 				//musica = musica.replaceFirst("\\]", "\\)");
-				musica = musica.replaceAll("_", " ");
-				musica = musica.replaceFirst(" ", " - ");
+//				musica = musica.replaceAll("_", " ");
+				musica = musica.replaceFirst("Cat Power - Wanderer - ", "");
 				//musica = musica.replaceFirst(musica.substring(musica.length()-13, musica.length()-4), "");
 				//musica = musica.replaceFirst(musica.substring(0, 2), "");
 				//Remover string no final do nome do arquivo
 				
-				musica = musica.replaceFirst("-RCD", "");
+//				musica = musica.replaceFirst("-RCD", "");
 				//musica = musica.replaceFirst("Deep Purple -", "");
 				//musica = musica.replaceFirst("-deep_purple-", " - ");
 				//musica = musica.replaceFirst(musica.substring(0, 1), "");
@@ -231,12 +231,12 @@ public class FileHandler {
 				File newNameFile = null;
 				for (int j = 0; j < listOfFiles[i].getName().length(); j++){
 					//Condicional responsável por procurar a primeira letra de uma palavra e se é um caracter válido
-					if (listOfFiles[i].getName().substring(j, j+1).equals(" ") && !listOfFiles[i].getName().substring(j+1, j+2).equals("(")){
+					if (listOfFiles[i].getName().substring(j, j+1).equals(" ") && !listOfFiles[i].getName().substring(j+1, j+2).equals("(") && !listOfFiles[i].getName().substring(j+1, j+2).equals("[")){
 						String replaceString = newName.substring(j, j+2); //Obtendo a primeira letra da palavra
 						newName = newName.replaceFirst(replaceString, replaceString.toUpperCase()); //tornando a primeira letra da palavra maiúscula
 						newNameFile = new File(stringPath + newName); //Obtendo o caminho completo do arquivo
 						//Se o caracter encontrado for um parênteses, tornar o próximo caracter maiúsculo
-					} else if (listOfFiles[i].getName().substring(j, j+1).equals("(")){
+					} else if (listOfFiles[i].getName().substring(j, j+1).equals("(") && listOfFiles[i].getName().substring(j, j+1).equals("[")){
 						String replaceString = newName.substring(j, j+2);
 						newName = newName.replaceFirst('\\' + replaceString, '\\' + replaceString.toUpperCase());
 						newNameFile = new File(stringPath + newName);
